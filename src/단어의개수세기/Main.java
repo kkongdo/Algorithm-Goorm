4c1b2b2393054dccbe19316691fde028b3686685
kkongdo <kkongdoes@naver.com>
2024-11-11 00:41:26 +0900

    Level1: 단어의 개수 세기

---
+++ src/단어의개수세기/Main.java
@@ -0,0 +1,12 @@
+package 단어의개수세기;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		System.out.println(st.countTokens());
+	}
+}
