53ac870079742df9de5694d16b155860ae8e5a54
kkongdo <kkongdoes@naver.com>
2024-11-19 18:54:20 +0900

    Level1: 뱀이 지나간 자리(다른 풀이 방법)

--- src/뱀이지나간자리/Main.java
+++ src/뱀이지나간자리/Main.java
@@ -1,59 +1,31 @@
-package 뱀이지나간자리;
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		char[][] nums = new char[N][M];
+        char[][] nums = new char[N][M];
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				nums[i][j] = '.';
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                Arrays.fill(nums[i], '#');
+            } else {
+                Arrays.fill(nums[i], '.');
+                if ((i / 2) % 2 == 0) {
+                    nums[i][M - 1] = '#';
+                } else {
+                    nums[i][0] = '#';
+                }
+            }
+        }
 
-		boolean flag = true;
-		for (int i = 0; i < N; i++) {
-			if (i % 2 == 0) {
-				if (flag == true) {
-					for (int j = 0; j < M; j++) {
-						nums[i][j] = '#';
-					}
-					flag = false;
-					continue;
-				}
-
-				if (flag == false) {
-					for (int j = M - 1; j >= 0; j--) {
-						nums[i][j] = '#';
-					}
-					flag = true;
-				}
-			}
-			if (i % 2 == 1) {
-				if (i < N) {
-					if (flag == true) {
-						nums[i][0] = '#';
-					}
-					if (flag == false) {
-						nums[i][M - 1] = '#';
-					}
-				}
-			}
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				System.out.print(nums[i][j]);
-			}
-			System.out.println();
-		}
-	}
+        for (int i = 0; i < N; i++) {
+            System.out.println(new String(nums[i]));
+        }
+    }
 }
