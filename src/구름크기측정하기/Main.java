6de7dba77e9479ccbc890d85579d5702f8920ef7
kkongdo <kkongdoes@naver.com>
2024-11-24 15:27:30 +0900

    Level01: 구름크기측정하기

---
+++ src/구름크기측정하기/Main.java
@@ -0,0 +1,16 @@
+package 구름크기측정하기;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(N * M);
+	}
+}
