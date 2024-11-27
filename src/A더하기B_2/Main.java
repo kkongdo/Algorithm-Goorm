1904dff82e1efe80439bc81de6109babd9d74243
kkongdo <kkongdoes@naver.com>
2024-11-27 13:43:00 +0900

    LEVEL01: A + B(2)

---
+++ src/A더하기B_2/Main.java
@@ -0,0 +1,16 @@
+package A더하기B_2;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+		
+		String result = String.format("%.6f", A + B);
+		System.out.println(result);
+	}
+}
